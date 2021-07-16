55be207d998abe5bdf23f17a5bacfde0bff67376
pokeMike <53858216+pokeMike@users.noreply.github.com>
2021-07-17 01:19:03 +0530

    Landscape

---
+++ Source/SurvivalHorror/SurvivalHorror.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivalHorror.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivalHorror, "SurvivalHorror" );
---
+++ Source/SurvivalHorror/SurvivalHorrorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivalHorrorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVALHORROR_API ASurvivalHorrorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
